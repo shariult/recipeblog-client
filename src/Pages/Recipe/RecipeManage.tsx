cf731605e62739c858b652ed57b934f1e858169c
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2023-08-17 19:07:34 +0600

    add form in recipe manage

--- src/Pages/Recipe/RecipeManage.tsx
+++ src/Pages/Recipe/RecipeManage.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Form from "../../components/ui/Form";
+import Input from "../../components/ui/Input";
+import Button from "../../components/ui/Button";
+
+import styles from "./RecipeManage.module.scss";
 
 function RecipeManage() {
-  return <div>RecipeManage</div>;
+  return (
+    <div>
+      <div>new recipe</div>
+      <div>
+        <h2 className="heading-2">New Recipe</h2>
+        <Form>
+          <Input type="text" name="title" />
+          <div className="form__group">
+            <Button type="submit" variant="primary">
+              Add Recipe
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default RecipeManage;
--- src/Pages/User/UserProfile.tsx
+++ src/Pages/User/UserProfile.tsx
@@ -1,7 +1,53 @@
 import React from "react";
 
+import styles from "./UserProfile.module.scss";
+import RecipeList from "../../components/Recipe/RecipeList";
+
 function UserProfile() {
-  return <div>Profile</div>;
+  return (
+    <section className={styles["profile"]}>
+      <div className={styles["profile__header"]}>
+        <div className={styles["profile__img-box"]}>
+          <img
+            src="/users/user-2.jpg"
+            alt="oval shaped object inside colored rectangle"
+            className={styles["profile__img"]}
+          />
+        </div>
+        <div className={styles["profile__info"]}>
+          <h2 className="heading-2">Profile</h2>
+          <div className={styles["profile__info-box"]}>
+            <span className={styles["profile__label"]}>Username</span>
+            <p className={styles["profile__text"]}>@Muhammed</p>
+          </div>
+          <div className={styles["profile__info-box"]}>
+            <span className={styles["profile__label"]}>Full Name</span>
+            <p className={styles["profile__text"]}>Muhammed Admed</p>
+          </div>
+          <div className={styles["profile__info-box"]}>
+            <span className={styles["profile__label"]}>Email</span>
+            <p className={styles["profile__text"]}>sdsds@dfdfd.com</p>
+          </div>
+          <div className={styles["profile__info-box"]}>
+            <span className={styles["profile__label"]}>Bio</span>
+            <p className={styles["profile__text"]}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur ipsam dolor, magni amet ipsa, deleniti dignissimos
+              laboriosam laudantium necessitatibus illum quam nam! Nesciunt
+              vitae ipsum minima earum aut, maxime repellendus laudantium rerum
+              id accusamus ipsam, sunt deleniti aliquid, corporis ab.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles["profile__recipes"]}>
+        <h3 className={`heading-3 ${styles["profile__recipes-title"]}`}>
+          Recipes
+        </h3>
+        <RecipeList />
+      </div>
+    </section>
+  );
 }
 
 export default UserProfile;
