7f1d459e90063bce5786023d971df7528eedc506
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2023-08-18 18:16:28 +0600

    complete header of landing page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/layout/Header";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default Home;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Button from "../ui/Button";
+
+import styles from "./Header.module.scss";
+import headerImg from "../../assets/img/hero.jpg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles["header"]}>
+      <div className={styles["header__img-box"]}>
+        <img
+          src={headerImg}
+          alt="food on a table"
+          className={styles["header__img"]}
+        />
+      </div>
+      <div className={styles["header__content"]}>
+        <h1 className={"heading-1 " + styles["header__title"]}>
+          Whisk & Delight: A Culinary Adventure Through Flavors
+        </h1>
+        <p className={styles["header__sub-title"]}>
+          Savor Every Bite. Embark on a Gastronomic Journey with Our Delectable
+          Recipes, Culinary Tips, and Flavorful Tales
+        </p>
+        <Button variant="primary">Sign Up Now</Button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
