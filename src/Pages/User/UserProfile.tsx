dbe444af57199d2995fe1a9792228cf9fd3e2cd4
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2023-08-18 18:56:38 +0600

    improve user profile and change colors

--- src/Pages/User/UserProfile.tsx
+++ src/Pages/User/UserProfile.tsx
@@ -15,29 +15,32 @@ function UserProfile() {
           />
         </div>
         <div className={styles["profile__info"]}>
-          <h2 className="heading-2">Profile</h2>
-          <div className={styles["profile__info-box"]}>
-            <span className={styles["profile__label"]}>Username</span>
-            <p className={styles["profile__text"]}>@Muhammed</p>
-          </div>
-          <div className={styles["profile__info-box"]}>
-            <span className={styles["profile__label"]}>Full Name</span>
-            <p className={styles["profile__text"]}>Muhammed Admed</p>
-          </div>
-          <div className={styles["profile__info-box"]}>
-            <span className={styles["profile__label"]}>Email</span>
-            <p className={styles["profile__text"]}>sdsds@dfdfd.com</p>
-          </div>
-          <div className={styles["profile__info-box"]}>
-            <span className={styles["profile__label"]}>Bio</span>
-            <p className={styles["profile__text"]}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur ipsam dolor, magni amet ipsa, deleniti dignissimos
-              laboriosam laudantium necessitatibus illum quam nam! Nesciunt
-              vitae ipsum minima earum aut, maxime repellendus laudantium rerum
-              id accusamus ipsam, sunt deleniti aliquid, corporis ab.
-            </p>
-          </div>
+          <h2 className="heading-2">Muhammed Admed</h2>
+          <ul className={styles["profile__list"]}>
+            <li className={styles["profile__item"]}>
+              <span className={styles["profile__label"]}>Username</span>
+              <p className={styles["profile__text"]}>@Muhammed</p>
+            </li>
+            <li className={styles["profile__item"]}>
+              <span className={styles["profile__label"]}>Email</span>
+              <p className={styles["profile__text"]}>sdsds@dfdfd.com</p>
+            </li>
+            <li className={styles["profile__item"]}>
+              <span className={styles["profile__label"]}>Gender</span>
+              <p className={styles["profile__text"]}>Male</p>
+            </li>
+            <li className={styles["profile__item"]}>
+              <span className={styles["profile__label"]}>Occupation</span>
+              <p className={styles["profile__text"]}>Teacher</p>
+            </li>
+          </ul>
+          <p className={styles["profile__about"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ipsam dolor, magni amet ipsa, deleniti dignissimos
+            laboriosam laudantium necessitatibus illum quam nam! Nesciunt vitae
+            ipsum minima earum aut, maxime repellendus laudantium rerum id
+            accusamus ipsam, sunt deleniti aliquid, corporis ab.
+          </p>
         </div>
       </div>
       <div className={styles["profile__recipes"]}>
